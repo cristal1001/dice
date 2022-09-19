b3ad1066a5d55ea73b34167de4f07009758cab25
Cristal Blanco <cristalblanco@Luis-MacBook-Pro.local>
2022-09-19 16:52:33 -0400

    first commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Cristal Blanco on 7/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
